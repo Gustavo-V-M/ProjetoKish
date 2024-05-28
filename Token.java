42e779c737b432fb19a5fdff31ea41ba95f87f12
FloFlorio <146960599+FloFlorio@users.noreply.github.com>
2024-05-27 22:45:17 -0300

    Update Token.java

--- Token.java
+++ Token.java
@@ -1,3 +1,8 @@
+// Guilherme Florio Vieira 10409698
+// Arthur Ryuiti Sato Furukawa 10409106
+// Gabriel Fuentes de Freitas Yamashita 10408876
+// Gustavo Vilela Mitraud 10400866
+
 public class Token {
     private static int nextId = 0;
     private int id;
